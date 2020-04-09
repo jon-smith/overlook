e88b3efe6b81479f40f3018f203fe9a201334a4d
jon-smith <jonsmith24601@outlook.com>
2020-04-09 11:22:18 +0100

    added parallax scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Parallax } from 'react-parallax';
 import Logo from './components/logo';
+import background from './img/background1.jpg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import './glitch.scss';
@@ -7,15 +9,27 @@ import './glitch.scss';
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<Logo />
-				WELCOME TO THE
-				<div className="glitch" data-text="OVERLOOK">
-					OVERLOOK
+			<Parallax
+				blur={{ min: -30, max: 50 }}
+				bgImage={background}
+				bgImageAlt="background"
+				strength={300}
+			>
+				<div className="app-page">
+					<Logo />
+					WELCOME TO THE
+					<div className="glitch" data-text="OVERLOOK">
+						OVERLOOK
+					</div>
+					HOTEL
 				</div>
-				HOTEL
-				<Counter />
-			</header>
+			</Parallax>
+			<Parallax blur={1} strength={200}>
+				<div className="app-page">
+					Redux example
+					<Counter />
+				</div>
+			</Parallax>
 		</div>
 	);
 }
