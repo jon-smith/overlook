1a14597a0097353bb6cb38ffe6783a30949931e5
jon-smith <jonsmith24601@outlook.com>
2020-04-09 08:58:45 +0100

    added eslint and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,19 @@ import './App.css';
 import './glitch.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        WELCOME TO THE
-        <div className="glitch" data-text="OVERLOOK">
-          OVERLOOK
-        </div>
-        HOTEL
-        <Counter />        
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				WELCOME TO THE
+				<div className="glitch" data-text="OVERLOOK">
+					OVERLOOK
+				</div>
+				HOTEL
+				<Counter />
+			</header>
+		</div>
+	);
 }
 
 export default App;
