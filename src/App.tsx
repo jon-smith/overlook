3048c6478f19039a3a8074d7fe7cac187d9020da
jon-smith <jonsmith24601@outlook.com>
2020-04-14 08:43:34 +0100

    improved door animation

--- src/App.tsx
+++ src/App.tsx
@@ -29,40 +29,111 @@ const FixedIconContainer = <T extends {}>(props: {
 	</div>
 );
 
-const bottomOffsetFromProp = (p: number) => {
-	const zeroToFive = p * 5;
-	if (zeroToFive < 2) {
-		return (zeroToFive / 2) * 55 - 10;
+type Stage = 'door' | 'axe' | 'face' | 'exit';
+
+const stageToIndex = (s: Stage) => {
+	switch (s) {
+		case 'door':
+			return 0;
+		case 'axe':
+			return 1;
+		case 'face':
+			return 2;
+		case 'exit':
+			return 3;
+		default:
+			return 4;
 	}
-	if (zeroToFive < 4) {
-		return 45;
+};
+
+type StageAndRelativeProgress = {
+	stage: Stage;
+	relativeP: number;
+};
+
+// Calculate the 'stage' of the animation and the relative progress within that stage
+const stageAndRelativeProgress = (p: number): StageAndRelativeProgress => {
+	const N = 7;
+	const zeroToN = p * N;
+	if (zeroToN < 2) {
+		return { stage: 'door', relativeP: zeroToN / 2 };
 	}
-	return (zeroToFive - 4) * 45 + 55;
+	if (zeroToN < 4) {
+		return { stage: 'axe', relativeP: (zeroToN - 2) / 2 };
+	}
+	if (zeroToN < 6) {
+		return { stage: 'face', relativeP: (zeroToN - 4) / 2 };
+	}
+	return { stage: 'exit', relativeP: zeroToN - (N - 1) };
+};
+
+const bottomOffsetAndDoorSize = (p: StageAndRelativeProgress) => {
+	if (p.stage === 'door') {
+		return { bottomOffset: p.relativeP * 55 - 10, doorSize: p.relativeP * 5 };
+	}
+	if (p.stage === 'exit') {
+		return { bottomOffset: p.relativeP * 55 + 45, doorSize: 5 };
+	}
+	return { bottomOffset: 45, doorSize: 5 };
+};
+
+const calcAxeOffset = (p: StageAndRelativeProgress) => {
+	if (p.stage === 'axe' || p.stage === 'face') {
+		const maxOffset = p.stage === 'face' ? 90 : 50;
+
+		if (p.relativeP <= 1 / 3) return p.relativeP * 3 * maxOffset;
+		if (p.relativeP <= 2 / 3) return maxOffset;
+
+		return (1 - p.relativeP) * 3 * maxOffset;
+	}
+
+	return 0;
+};
+
+const calcFaceOffset = (p: StageAndRelativeProgress) => {
+	if (p.stage === 'face') {
+		const maxOffset = 60;
+		if (p.relativeP <= 1 / 3) return p.relativeP * 3 * maxOffset;
+		if (p.relativeP <= 2 / 3) return maxOffset;
+
+		return (1 - p.relativeP) * 3 * maxOffset;
+	}
+
+	return 0;
 };
 
 const DoorScene = (props: { percentage: number }) => {
 	const { percentage } = props;
-	const normalisedP = (percentage - 1 / 3) / (2 / 3);
+	// The start percentage depends on the height of the div in the current parallax section
+	const startP = 0.375;
+	const normalisedP = (percentage - startP) / (1 - startP);
 	const hideAll = normalisedP <= 0.01 || normalisedP >= 0.99;
 	if (hideAll) return <div />;
 
-	const bottomOffset = bottomOffsetFromProp(normalisedP);
-	const doorSize = Math.min(5, (5 * bottomOffset) / 45);
-	const axeOffset = Math.min(50, Math.max(0.0, (normalisedP - 0.5) * 300));
+	const stageAndP = stageAndRelativeProgress(normalisedP);
+	const stageIndex = stageToIndex(stageAndP.stage);
 
+	const { bottomOffset, doorSize } = bottomOffsetAndDoorSize(stageAndP);
+	const axeOffset = calcAxeOffset(stageAndP);
+	const faceOffset = calcFaceOffset(stageAndP);
 	return (
 		<div>
-			{normalisedP >= 0.5 && (
+			{stageIndex >= stageToIndex('face') && (
+				<FixedIconContainer
+					icon={angry}
+					width="3em"
+					leftOffsetPx={faceOffset}
+					bottomOffsetPc={bottomOffset + 6}
+				/>
+			)}
+			{stageIndex >= stageToIndex('axe') && (
 				<FixedIconContainer
 					icon={axe}
 					width="3em"
 					hFlip={true}
 					leftOffsetPx={axeOffset}
-					bottomOffsetPc={bottomOffset}
+					bottomOffsetPc={bottomOffset + 3}
 				/>
-			)}
-			{normalisedP >= 0.5 && (
-				<FixedIconContainer icon={angry} width="3em" bottomOffsetPc={bottomOffset} />
 			)}
 			<FixedIconContainer icon={door} width={`${doorSize}em`} bottomOffsetPc={bottomOffset} />
 		</div>
@@ -95,6 +166,7 @@ function App() {
 						<div className={styles.appPage}>
 							<DoorScene percentage={percentage} />
 						</div>
+						<div className={styles.appPage} />
 						<div className={styles.appPage} />
 						<div className={styles.appPage} />
 					</>
