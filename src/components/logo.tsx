434673ead681ba9c6250bc24a9152dff42e48165
jon-smith <jonsmith24601@outlook.com>
2020-04-17 14:53:48 +0100

    carpet pattern in SVG

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import styles from './logo.module.css';
 
-const buildHexPoints = (radius: number, midX: number, midY: number) => {
-	return [0, 1, 2, 3, 4, 5, 6]
-		.map((n, i) => {
-			const angleDeg = 60 * i - 30;
-			const angleRad = (Math.PI / 180) * angleDeg;
-			return [midX + radius * Math.cos(angleRad), midY + radius * Math.sin(angleRad)];
-		})
-		.map(p => p.join(','))
-		.join(' ');
+const hexPointFromEdgeIndex = (
+	index: number,
+	radius: number,
+	midX: number,
+	midY: number
+): [number, number] => {
+	const angleDeg = 60 * index - 30;
+	const angleRad = (Math.PI / 180) * angleDeg;
+	return [midX + radius * Math.cos(angleRad), midY + radius * Math.sin(angleRad)];
+};
+
+const buildHexPoints = (radius: number, midX: number, midY: number): [number, number][] => {
+	return [0, 1, 2, 3, 4, 5, 6].map((n, i) => hexPointFromEdgeIndex(i, radius, midX, midY));
+};
+
+const pointsToString = (points: [number, number][]) => points.map(p => p.join(',')).join(' ');
+
+const buildHexPointsString = (radius: number, midX: number, midY: number) => {
+	return pointsToString(buildHexPoints(radius, midX, midY));
 };
 
 const SVGElements = (props: { size: number }) => {
 	const { size } = props;
 	const buildHex = (radius: number, color: string) => {
-		const p = buildHexPoints(radius, size * 0.5, size * 0.5);
+		const p = buildHexPointsString(radius, size * 0.5, size * 0.5);
 		return <polygon points={p} fill={color} />;
 	};
 
@@ -27,6 +37,71 @@ const SVGElements = (props: { size: number }) => {
 			{buildHex(outerSize * 0.8, '#ffb347')}
 			{buildHex(outerSize * 0.6, '#2e1c01')}
 			{buildHex(outerSize * 0.4, '#ff443a')}
+		</>
+	);
+};
+
+const hexMinorToMajorDimension = (w: number) => (w * 2) / Math.sqrt(3);
+
+const BackgroundSVGElements = (props: { width: number }) => {
+	const { width } = props;
+
+	const hexWidth = (width * 8) / 7;
+	const lineThickness = hexWidth / 8;
+	const hexRad = hexMinorToMajorDimension(hexWidth) * 0.5;
+	const offsetX = 0; // -width * 0.125;
+	const offsetY = 0;
+
+	const buildMainLogo = (centreX: number, centreY: number, includeOuter: boolean) => {
+		const buildHex = (radius: number, color: string) => {
+			const p = buildHexPointsString(radius, centreX + offsetX, centreY + offsetY);
+			return <polygon points={p} fill={color} />;
+		};
+		return (
+			<>
+				{includeOuter && buildHex(hexRad, 'black')}
+				{buildHex(hexRad * 0.75, '#ffb347')}
+				{buildHex(hexRad * 0.5, 'black')}
+				{buildHex(hexRad * 0.25, '#ff443a')}
+			</>
+		);
+	};
+
+	const buildFillLine = (centreX: number, top: number) => {
+		const x = centreX - lineThickness * 0.5;
+		return (
+			<rect x={x + offsetX} y={top + offsetY} width={lineThickness} height={hexRad} fill="black" />
+		);
+	};
+
+	const hOffset = hexRad * 0.75;
+	const vOffset = hexRad * 0.82;
+
+	const topX = width * 0.5;
+	const topY = width * 0.5;
+
+	const leftX = topX - hOffset;
+	const leftY = topY + vOffset;
+	const topLeftY = leftY - 2.6 * vOffset;
+
+	const rightX = topX + hOffset;
+	const rightY = leftY;
+	const topRightY = topLeftY;
+
+	const bottomX = topX;
+	const bottomY = topY + vOffset * 2;
+
+	return (
+		<>
+			{buildMainLogo(topX, topY, true)}
+			{buildMainLogo(leftX, leftY, false)}
+			{buildMainLogo(rightX, rightY, false)}
+			{buildMainLogo(leftX, topLeftY, false)}
+			{buildMainLogo(rightX, topRightY, false)}
+			{buildMainLogo(bottomX, bottomY, false)}
+			{buildFillLine(topX, topY + hexRad * 0.25)}
+			{buildFillLine(leftX, leftY - 1.25 * hexRad)}
+			{buildFillLine(rightX, rightY - 1.25 * hexRad)}
 		</>
 	);
 };
@@ -44,11 +119,20 @@ export const Logo = () => {
 };
 
 export const LogoBackground = () => {
+	const width = 40;
+	const height = width * 1.41;
 	return (
 		<svg width="100%" height="100%">
 			<defs>
-				<pattern id="pattern" x="0" y="0" width="100" height="100" patternUnits="userSpaceOnUse">
-					<SVGElements size={100} />
+				<pattern
+					id="pattern"
+					x="0"
+					y="0"
+					width={width}
+					height={height}
+					patternUnits="userSpaceOnUse"
+				>
+					<BackgroundSVGElements width={width} />
 				</pattern>
 			</defs>
 			<rect x="0" y="0" width="100%" height="100%" fill="url(#pattern)" />
