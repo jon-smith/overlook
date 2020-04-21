cd71fea6cce0494233ec71761cd562fddc80c953
jon-smith <jonsmith24601@outlook.com>
2020-04-21 11:04:34 +0100

    replace text in leave a comment

--- src/pages/typewriter-scene.tsx
+++ src/pages/typewriter-scene.tsx
@@ -3,6 +3,15 @@ import React, { useState } from 'react';
 import { InlineIcon } from '@iconify/react';
 import keyboardIcon from '@iconify/icons-twemoji/keyboard';
 
+const textToRepeat = 'all work and no play makes Jack a dull boy ';
+
+const alterText = (text: string) => {
+	const characters = text.length;
+	const repeats = Math.floor(characters / textToRepeat.length);
+	const extra = characters % textToRepeat.length;
+	return new Array(repeats).fill(textToRepeat).join('') + textToRepeat.slice(0, extra);
+};
+
 const TypewriterScene = () => {
 	const [text, setText] = useState('');
 
@@ -14,7 +23,11 @@ const TypewriterScene = () => {
 				<InlineIcon icon={keyboardIcon} />
 			</div>
 			<div>
-				<textarea value={text} onChange={e => setText(e.target.value)} />
+				<textarea
+					style={{ minHeight: '30vh', minWidth: '50vw' }}
+					value={alterText(text)}
+					onChange={e => setText(e.target.value)}
+				/>
 			</div>
 		</div>
 	);
