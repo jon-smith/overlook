2bf4b8ce9adbd3965533886f68a32b4d95a1e40a
jon-smith <jonsmith24601@outlook.com>
2020-04-21 10:56:57 +0100

    auto hide div using y scroll enter div

--- src/components/div-auto-hide-content.tsx
+++ src/components/div-auto-hide-content.tsx
@@ -1,32 +1,26 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { useWindowScroll } from '@react-hook/window-scroll';
+import React, { useState } from 'react';
+import DivWithOnYScrollEnter from './div-on-scroll-to-visible';
 
 interface Props {
 	scrollFps?: number;
 }
 
-const elementInWithinYRange = (element: HTMLDivElement) => {
-	const { top, bottom } = element.getBoundingClientRect();
-	return bottom > 0 && top < window.innerHeight;
-};
-
 // Div that hides the content when it has scrolled out of y-range
 const DivAutoHideContent = (
 	props: React.PropsWithChildren<Props & React.ComponentProps<'div'>>
 ) => {
-	const { children, scrollFps, ...rest } = props;
+	const { children, ...rest } = props;
 
-	const divRef = useRef<HTMLDivElement>(null);
-	const scrollPos = useWindowScroll(scrollFps);
 	const [show, setShow] = useState(false);
-	useEffect(() => {
-		if (divRef.current) setShow(elementInWithinYRange(divRef.current));
-	}, [scrollPos]);
 
 	return (
-		<div ref={divRef} {...rest}>
+		<DivWithOnYScrollEnter
+			onLeaveYRange={() => setShow(false)}
+			onEnterYRange={() => setShow(true)}
+			{...rest}
+		>
 			{show && children}
-		</div>
+		</DivWithOnYScrollEnter>
 	);
 };
 
